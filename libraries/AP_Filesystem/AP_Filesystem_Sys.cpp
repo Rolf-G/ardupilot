0ceb9715c26ed073fd95f508af5cbe64b70f93d7
Andy Piper <github@andypiper.com>
2021-01-05 21:18:49 +0000

    AP_Filesystem: add @SYS/uarts.txt

--- libraries/AP_Filesystem/AP_Filesystem_Sys.cpp
+++ libraries/AP_Filesystem/AP_Filesystem_Sys.cpp
@@ -34,6 +34,7 @@ static const SysFileList sysfs_file_list[] = {
     {"threads.txt"},
     {"tasks.txt"},
     {"dma.txt"},
+    {"uarts.txt"},
 #if HAL_MAX_CAN_PROTOCOL_DRIVERS
     {"can_log.txt"},
     {"can0_stats.txt"},
@@ -90,6 +91,9 @@ int AP_Filesystem_Sys::open(const char *fname, int flags)
     }
     if (strcmp(fname, "dma.txt") == 0) {
         hal.util->dma_info(*r.str);
+    }
+    if (strcmp(fname, "uarts.txt") == 0) {
+        hal.util->uart_info(*r.str);
     }
 #if HAL_MAX_CAN_PROTOCOL_DRIVERS
     int8_t can_stats_num = -1;
